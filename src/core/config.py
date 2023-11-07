9bc266fc32bc5d3d25f7b557bd16f6e5a7214bd0
Aleksandr Rogachev <alxrogachev@gmail.com>
2023-11-07 22:21:31 +0400

    add config file

---
+++ src/core/config.py
@@ -0,0 +1,34 @@
+import os
+
+from dotenv import load_dotenv
+from pydantic import PostgresDsn
+from pydantic_settings import BaseSettings
+
+
+load_dotenv()
+
+
+class AppSettings(BaseSettings):
+    """
+    Настройки приложения.
+
+    :param is_debug: Режим отладки.
+    :param app_title: Название приложения.
+    :param database_dsn: Строка подключения к базе данных.
+    """
+    is_debug: bool
+    app_title: str
+    DATABASE_DSN: PostgresDsn
+
+
+    class Config:
+        """
+        Настройки конфигурации.
+
+        :param env_file: Файл с настройками.
+        """
+        env_file = '.env'
+
+
+app_settings: AppSettings = AppSettings()
+BASE_DIR: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
--- src/main.py
+++ src/main.py
@@ -1,2 +1,23 @@
-if __name__ == "__main__":
-    pass
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+
+# from api.v1 import base
+from core.config import app_settings
+
+app: FastAPI = FastAPI(
+    title=app_settings.app_title,
+    docs_url='/api/openapi',
+    openapi_url='/api/openapi.json',
+    default_response_class=ORJSONResponse,
+)
+
+# app.include_router(base.api_router, prefix='/api/v1')
+
+if __name__ == '__main__':
+    uvicorn.run(
+        'main:app',
+        host='0.0.0.0',
+        port=8000,
+        reload=True,
+    )
