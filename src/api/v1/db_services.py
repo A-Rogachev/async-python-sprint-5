9b4155f58946b34a51c698434f3eec13ff780b03
Aleksandr Rogachev <alxrogachev@gmail.com>
2023-11-09 22:01:37 +0400

    some refactoring

--- src/api/v1/db_services.py
+++ src/api/v1/db_services.py
@@ -13,11 +13,18 @@ async def ping() :
     """
     Получение информации о времени доступа ко всем связанным сервисам.
     """
-    try:
-        raise psycopg2.OperationalError
-    except psycopg2.OperationalError:
-        postgresql_response_time = 'not available'
+    postgresql_response_time: str = await get_postgresql_ping_value()
+    redis_response_time: str = await get_redis_ping_value()
+    return DbServicesPing(
+        postgresql_db=postgresql_response_time,
+        redis_cache=f'{redis_response_time} seconds',
+    )
 
+
+async def get_redis_ping_value() -> str:
+    """
+    Время доступа к базе данных для кеширования токенов пользователей.
+    """
     redis_start_time = time.time()
     try:
         with redis.Redis(host='localhost', port=6379, db=0) as redis_client:
@@ -25,8 +32,16 @@ async def ping() :
             redis_response_time = round(time.time() - redis_start_time, 5)
     except redis.ConnectionError:
         redis_response_time = 'not available'
+    return redis_response_time
 
-    return DbServicesPing(
-        postgresql_db=postgresql_response_time,
-        redis_cache=f'{redis_response_time} seconds',
-    )
+
+async def get_postgresql_ping_value() -> str:
+    """
+    Время доступа к базе данных postgresql.
+    """
+    postgresql_start_time = time.time()
+    try:
+        raise psycopg2.OperationalError
+    except psycopg2.OperationalError:
+        postgresql_response_time = 'not available'
+    return postgresql_response_time
